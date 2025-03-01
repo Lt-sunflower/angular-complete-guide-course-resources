ee2e4e970f62b562e3faf94f4ff2d26f941eee79
Luke Toh <luketoh.practice@gmail.com>
2025-03-01 13:52:47 +0800

    Checkpoint: Lecture 32

--- code-snapshots/02-essentials/01-starting-project/src/app/components/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input, input, signal } from '@angular/core'; //Input is a decorator from angular, input is a special function from the library
 import { DUMMY_USERS } from './dummy-users';
 
 const random_int = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,16 +12,34 @@ const random_int = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[random_int];
+  // @Input is decorator from angular
+  // !: is a typescript convention to tell the compiler that the variable will be initialized later
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  
+  // signal, so include (brackets) again
+  avatar = input.required<string>();
+  name = input<string>('NAME');
 
   get imagePath() {
-    return './assets/users/' + this.selectedUser.avatar;
+    return './assets/users/' + this.avatar();
   }
 
+
+  // pass initial value
+  // selectedUser = signal(DUMMY_USERS[random_int]);
+  // // computed signal, all signals are called as method in html
+  // imagePath = computed(() => './assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath() {
+  //   return './assets/users/' + this.selectedUser.avatar;
+  // }
+
   onSelectUser() {
-    const random_int = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[random_int];
-    // console.log(random_int)
+    // const random_int = Math.floor(Math.random() * DUMMY_USERS.length);
+    // // this.selectedUser = DUMMY_USERS[random_int];
+    // this.selectedUser.set(DUMMY_USERS[random_int]);
+    // // console.log(random_int)
   }
 
 }
